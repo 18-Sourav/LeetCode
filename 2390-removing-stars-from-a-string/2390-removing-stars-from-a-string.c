ae672fd3af5923e56d9192ae3263cb052d1e5691
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-06-24 00:01:46 +0530

    Time: 11 ms (25.5%), Space: 14.9 MB (34.96%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.c
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.c
@@ -1,15 +1,15 @@
 char* removeStars(char* s) {
-    char *stack = malloc(sizeof(char)*strlen(s)+1);
+    char *res = malloc(sizeof(char)*strlen(s)+1);
     int top = -1;
     int i = 0;
     while(s[i])
     {
         if(s[i]!='*')
-            stack[++top] = s[i];
+            res[++top] = s[i];
         else
             top--;
         i++;        
     }
-    stack[++top]='\0';
-    return stack;
+    res[++top]='\0';
+    return res;
 }
