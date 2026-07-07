55c4f94b454e7c149826f273f7b2e2a7ae8c64ca
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-07-07 18:36:20 +0530

    Time: 14 ms (57.5%), Space: 14.2 MB (75%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.c
@@ -0,0 +1,35 @@
+int maxProduct(char** words, int wordsSize) {
+    
+    int max_prod = 0;
+    unsigned int arr[wordsSize] ;
+    unsigned int size[wordsSize] ;
+    for(int i=0;i<wordsSize;i++)
+    {
+        int j=0;
+        arr[i] = 0;
+        size[i] = 0;
+        while(words[i][j]!='\0')
+        {
+            unsigned char index = words[i][j]-'a';
+            arr[i]|= (1U<<index);
+            j++;
+        }
+        size[i] = j;
+    }
+    for(int i=0;i<wordsSize-1;i++)
+    {
+        for(int j=i+1;j<wordsSize;j++)
+        {
+            if((arr[i]&arr[j])==0)
+            {
+                int prod = size[i]*size[j];
+                if(prod>max_prod)
+                    max_prod = prod;
+            }
+        }
+    }
+    return max_prod;
+
+
+
+}
