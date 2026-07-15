54dc0a0f6ec755c7040a47fb6b58edd8fcda30fc
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-07-15 16:55:37 +0530

    Time: 0 ms (100%), Space: 24.9 MB (31.22%) - LeetHub

--- 0089-gray-code/0089-gray-code.c
+++ 0089-gray-code/0089-gray-code.c
@@ -2,12 +2,10 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* grayCode(int n, int* returnSize) {
-    int size = pow(2,n);
-    int *result = malloc(sizeof(int)*size);
+    *returnSize = 1<<n ;
+    int *result = malloc(sizeof(int)*(*returnSize));
 
-    *returnSize = size;
-
-    for(int i=0;i<size;i++)
+    for(int i=0;i<*returnSize;i++)
         result[i] = i ^ (i>>1);
     
     return result;
