05ac3fe88ed1a40c0d8f4accebb123c46b75d373
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-07-16 23:23:53 +0530

    Time: 4 ms (47.57%), Space: 8.7 MB (48.54%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.c
@@ -0,0 +1,36 @@
+int countSubstrings(char *s)
+{
+    int count = 0;
+
+    // Find length
+    int n = 0;
+    while (s[n] != '\0')
+        n++;
+
+    for (int i = 0; i < n; i++)
+    {
+        // Odd-length palindromes
+        int left = i;
+        int right = i;
+
+        while (left >= 0 && right < n && s[left] == s[right])
+        {
+            count++;
+            left--;
+            right++;
+        }
+
+        // Even-length palindromes
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < n && s[left] == s[right])
+        {
+            count++;
+            left--;
+            right++;
+        }
+    }
+
+    return count;
+}
