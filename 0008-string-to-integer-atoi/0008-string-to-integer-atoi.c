6b300f1296976013f3f151ab99e5ec0c0c8f25b4
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-07-22 21:00:33 +0530

    Time: 0 ms (100%), Space: 9.4 MB (29.33%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
@@ -0,0 +1,26 @@
+int myAtoi(char* s) {
+    int i = 0;
+    
+    while (s[i] == ' ') 
+    {
+        i++;
+    }
+    int sign = 1;
+    if (s[i] == '+' || s[i] == '-') 
+    {
+        if (s[i] == '-') sign = -1;
+        i++;
+    }
+    long result = 0;
+
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        int digit = s[i] - '0';
+        if (result > (INT_MAX - digit) / 10) 
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        result = result * 10 + digit;
+        i++;
+    }
+
+    return sign * result;
+}
