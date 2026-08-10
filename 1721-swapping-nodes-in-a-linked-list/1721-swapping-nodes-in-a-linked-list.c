627255957e491d5fb0d88082b4f5b24c2f4c1818
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-08-10 19:36:44 +0530

    Time: 0 ms (100%), Space: 53.6 MB (48.48%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.c
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.c
@@ -18,17 +18,15 @@ struct ListNode* swapNodes(struct ListNode* head, int k) {
     temp = head;
     //finding the kth node from the starting 
     for(int i=1;i<k;i++)
-    {
         temp = temp->next;
-    }
+    
     start_node = temp;
     temp = head;
     struct ListNode * end_node = NULL;
     //finding the kth element from the end
     for(int i=0;i<size-k;i++)
-    {
         temp = temp->next;
-    }
+    
     end_node = temp;
 
     int val = start_node->val;
