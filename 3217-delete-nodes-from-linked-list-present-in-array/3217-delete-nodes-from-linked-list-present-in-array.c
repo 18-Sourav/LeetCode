81c6d20284f8c0a60b1d08ca5ac3dd30604c1f52
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-08-11 21:29:08 +0530

    Time: 13 ms (61.45%), Space: 62.7 MB (72.29%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* modifiedList(int* nums, int numsSize, struct ListNode* head) {
+
+    bool hash[1000001] ={0};
+    for(int i=0;i<numsSize;i++)
+        hash[nums[i]] = 1;
+
+    struct ListNode dummy;
+    dummy.next = head;
+
+    struct ListNode* curr = &dummy;
+
+    while(curr->next!=NULL)
+    {
+        int found = 0;
+       
+        if(hash[curr->next->val])
+            curr->next = curr->next->next;
+        else
+            curr = curr->next;
+    }
+    return dummy.next; 
+
+}
