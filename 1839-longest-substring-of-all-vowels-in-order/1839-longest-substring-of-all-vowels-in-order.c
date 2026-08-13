33ea50ee8fd2902041f7c64fee1bf77be6b07c31
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-08-14 00:38:58 +0530

    Time: 28 ms (12.5%), Space: 14.9 MB (93.75%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.c
@@ -0,0 +1,25 @@
+int longestBeautifulSubstring(char* word) {
+    int left = 0,right = 1;
+    int max_len =0,vowel_cnt=1;
+
+    while(word[left])
+    {
+        if(word[right]<word[right-1])
+        {
+            left = right;
+            vowel_cnt = 1;
+        }
+        else
+        {
+            if(word[right]!=word[right-1])
+                vowel_cnt++;
+        }
+        int curr_len = right -left +1;
+
+        if(curr_len>max_len && vowel_cnt ==5)
+            max_len = curr_len;
+        if(word[right])    
+            right++;    
+    }
+    return max_len;
+}
