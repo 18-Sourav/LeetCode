02aa346b28a07e82a013780f77c9ec04622e371f
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-08-14 22:20:56 +0530

    Time: 1 ms (89.13%), Space: 89.3 MB (90.76%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.c
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.c
@@ -6,26 +6,25 @@
  * };
  */
 struct ListNode* mergeNodes(struct ListNode* head) {
-    struct ListNode dummy;
-    dummy.next = head;
 
-    struct ListNode* curr = &dummy;
-    curr= curr->next->next;
-    struct ListNode* temp = &dummy;
+    struct ListNode* curr = head;
+    curr= curr->next;
+    struct ListNode* temp = head;
     int sum = 0;
 
     while(curr!=NULL)
     {
         if(curr->val == 0)
         { 
-            temp = temp->next;
             temp->val = sum;
             sum = 0;
+            if(curr->next && curr->next->val!=0)
+                temp = temp->next;
         }
         else
             sum+=curr->val;
         curr = curr->next;
     }
     temp->next = NULL;
-    return dummy.next;
+    return head;
 }
