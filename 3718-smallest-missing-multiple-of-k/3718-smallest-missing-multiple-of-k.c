89086aba5548b31ee63c36100574c34614e6ba4c
18-Sourav <114919539+18-Sourav@users.noreply.github.com>
2026-08-25 19:16:17 +0530

    Time: 0 ms (100%), Space: 9.8 MB (34.21%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.c
@@ -0,0 +1,13 @@
+int missingMultiple(int* nums, int numsSize, int k) {
+    int arr[101] = {0};
+    for(int i=0;i<numsSize;i++)
+        arr[nums[i]]++;
+    
+    int size = k*numsSize,j=k;
+    for(;j<101;j+=k)
+    {
+        if(arr[j]==0)
+            break;
+    }
+    return j;
+}
